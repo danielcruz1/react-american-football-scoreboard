2432d9d891e3eb6c1a3df6fddf289bcb5669fc74
staccats <47672588+staccats@users.noreply.github.com>
2019-09-18 20:32:23 -0600

    stretch - useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -9,9 +9,18 @@ function App() {
   const [home, homeScore] = useState(0);
   const [visitor, visitorScore] = useState(0);
   // Stretch 
-  
-  const [team, teamName] = useState(0);
-   
+  const [timer, setTimer] = useState(60);
+
+  useEffect(() => {
+    const clock = setTimeout(() => {
+      if (timer > 0) {
+        setTimer(timer - 1)
+      } else {
+        clearTimeout(clock)
+      }
+    }, 1000)
+  },)
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -23,7 +32,7 @@ function App() {
 
             <div className="home__score">{home}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">00:{timer}</div>
           <div className="away">
             <h2 className="away__name">49ERS</h2>
             <div className="away__score">{visitor}</div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,8 +1,6 @@
 import React, {useState} from "react";
 import "./App.css";
 
-
-
 const BottomRow = () => {
 
   // Stretch
